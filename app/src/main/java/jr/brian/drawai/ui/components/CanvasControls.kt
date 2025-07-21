b580f7352183edbaedb69a80cfcef5579f103534
Brian Jr <thaballa79@gmail.com>
2025-07-20 20:01:21 -0400

    adds ability to toggle controls

--- app/src/main/java/jr/brian/drawai/ui/components/CanvasControls.kt
+++ app/src/main/java/jr/brian/drawai/ui/components/CanvasControls.kt
@@ -1,5 +1,6 @@
 package jr.brian.drawai.ui.components
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -14,11 +15,20 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Send
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,23 +48,43 @@ fun CanvasControls(
     onGenerateObjectToDraw: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var isResponseVisible by remember { mutableStateOf(true) }
+    val toggleResponseVisibility = { isResponseVisible = !isResponseVisible }
     Column(
-        modifier = Modifier.background(Color.DarkGray),
+        modifier = Modifier
+            .background(Color.DarkGray)
+            .clickable(onClick = toggleResponseVisibility),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            response ?: "Brainstorming what to draw...",
-            modifier = Modifier.padding(16.dp)
-        )
-        HorizontalDivider(
-            color = Color.White,
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
-        )
+        AnimatedVisibility(isResponseVisible) {
+            Text(
+                response ?: "Brainstorming what to draw...",
+                modifier = Modifier
+                    .padding(16.dp)
+
+            )
+        }
+        AnimatedVisibility(isResponseVisible) {
+            HorizontalDivider(
+                color = Color.White,
+                modifier = Modifier
+                    .padding(
+                        start = 16.dp,
+                        end = 16.dp,
+                        bottom = 16.dp
+                    )
+            )
+        }
         LazyRow(
             modifier = modifier
                 .fillMaxWidth()
-                .padding(start = 8.dp, end = 8.dp, top = 8.dp, bottom = 16.dp),
+                .padding(
+                    start = 8.dp,
+                    end = 8.dp,
+                    top = 8.dp,
+                    bottom = 16.dp
+                ),
             horizontalArrangement = Arrangement.spacedBy(
                 12.dp,
             ),
@@ -93,28 +123,40 @@ fun CanvasControls(
             }
 
             item {
-                Button(
+                IconButton(
                     enabled = isGeneratingObjToDraw,
-                    onClick = onGenerateObjectToDraw
+                    onClick = {
+                        isResponseVisible = true
+                        onGenerateObjectToDraw()
+                    }
                 ) {
-                    Text("Generate")
+                    Icon(
+                        imageVector = Icons.Outlined.Refresh,
+                        contentDescription = "Generate new object to draw",
+                    )
                 }
             }
 
             item {
-                Button(
+                IconButton(
                     enabled = isGeneratingObjToDraw,
                     onClick = onSubmitDrawing
                 ) {
-                    Text("Submit")
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Outlined.Send,
+                        contentDescription = "Submit drawing",
+                    )
                 }
             }
 
             item {
-                Button(
+                IconButton(
                     onClick = onClearCanvas
                 ) {
-                    Text("Clear")
+                    Icon(
+                        imageVector = Icons.Outlined.Delete,
+                        contentDescription = "Clear canvas",
+                    )
                 }
             }
 
